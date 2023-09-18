3be1c777ed4c4f25dd7a3051c81913e95c5483c6
shradha biradar <shradhab290@gmail.com>
2023-09-19 02:52:55 +0530

    iitm logo added

--- src/Pages/HomeFirstPage.js
+++ src/Pages/HomeFirstPage.js
@@ -1,10 +1,41 @@
 import React from "react";
+
 import "./homeFirstPage.css";
+import nirmaanLogo from "../images/NIRMAAN-LOGO-NEW 1.png";
+import iitMadrasLogo from "../images/iitmLogo.png";
 
 function HomeFirstPage() {
   return (
     <div>
-      
+      {/* nirman logo */}
+      <div className="nirmaan-logo">
+        <div className="this-is text">
+          <h1>
+            THIS <br></br> IS
+          </h1>
+        </div>
+        <div className="logo">
+          <img src={nirmaanLogo} />
+        </div>
+        <div className="text subtitle">
+          <h2>
+            THIS IS YOUR <br></br> SUBTITLE
+          </h2>
+          <p>
+            Nirmaan, The Pre-Incubator,<br></br>at IIT Madras is the first of{" "}
+            <br></br>its kind dedicated pre- <br></br>incubatore on a college{" "}
+            <br></br>campus in the country
+          </p>
+        </div>
+      </div>
+
+      {/* iit madras logo  */}
+      <div className="iitmlogo">
+        <img src={iitMadrasLogo} />
+      </div>
+      {/* social media  */}
+      <div></div>
+      <footer></footer>
     </div>
   );
 }
