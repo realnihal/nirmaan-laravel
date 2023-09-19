f5c23a8d377ba3e564d365d179a5802a9248d908
shradha biradar <shradhab290@gmail.com>
2023-09-20 00:03:42 +0530

    horizontal scrolling set up

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import HomeFirstPage from "./HomeFirstPage";
 import "./homeFirstPage.css";
+import HomeSecondPage from "./HomeSecondPage";
 
 function Home() {
   return (
-    <div className="horizontal-scroll-container">
-      <HomeFirstPage />
+    <div className="horizontal-scroll-container" style={{ overflowX: "auto" }}>
+      <div className="homeFirstPage">
+        <HomeFirstPage />
+      </div>
+      <div className="homeFirstPage">
+        <HomeFirstPage />
+      </div>
+      <div className="homeFirstPage">
+        <HomeFirstPage />
+      </div>
+      <div className="homeFirstPage">
+        <HomeFirstPage />
+      </div>
+      <div className="homeFirstPage">
+        <HomeFirstPage />
+      </div>
     </div>
   );
 }
