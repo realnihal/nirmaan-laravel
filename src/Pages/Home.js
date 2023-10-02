ef7358019bc3a9e4efbe986a5ffbf9d28cbef7d3
shradha biradar <shradhab290@gmail.com>
2023-10-03 03:58:05 +0530

    fourth page done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-import HomeFirstPage from "./HomeFirstPage";
 import "./homeFirstPage.css";
 import "./homeSecondPage.css";
 import "./homeThirdPage.css";
+import "./homeFourthPage.css";
+import HomeFirstPage from "./HomeFirstPage";
 import HomeSecondPage from "./HomeSecondPage";
 import HomeThirdPage from "./HomeThirdPage";
+import HomeFourthPage from "./HomeFourthPage";
 
 function Home() {
   return (
@@ -18,6 +20,9 @@ function Home() {
       </div>
       <div className="HomeThirdPage">
         <HomeThirdPage />
+      </div>
+      <div className="HomeFourthPage">
+        <HomeFourthPage />
       </div>
     </div>
   );
---
+++ src/Pages/HomeFourthPage.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import "./homeFourthPage.css";
+import dotted from "../images/Group 137.png";
+import iitmlogo_2 from "../images/iitmlogo-2.png";
+import nirmaanlogo from "../images/nirmaan-2.png";
+import leftImg from "../images/left-img.png";
+import bottom1 from "../images/bottom-1.png";
+import bottom2 from "../images/bottom-2.png";
+import rightImg from "../images/right-img.png";
+import leftCut from "../images/left-num.png";
+import bottom1Cut from "../images/bottom1-num.png";
+import bottom2Cut from "../images/bottom2-num.png";
+import rightCut from "../images/right-num.png";
+import logoNir from "../images/logo-nir.png"
+
+function HomeFourthPage() {
+  return (
+    <div className="fourth-page-container">
+      <div className="team-jour">
+        <div className="dotted-line">
+          <img src={dotted} alt="pattern" />
+          <img src={dotted} alt="pattern" />
+        </div>
+        <div className="nirmaan-numerical">
+          <div className="our-team common">
+            SEE<br></br>OUR<br></br>TEAM
+          </div>
+          <div className="numbers">
+            <div className="nir">NIRMAAN</div>
+            <div className="num">IN NUMBERS</div>
+          </div>
+          <div className="journey common">
+            SEE<br></br>OUR<br></br>JOURNEY
+          </div>
+        </div>
+        <div className="iitmlogo-2">
+          {" "}
+          <img src={iitmlogo_2} />
+        </div>
+      </div>
+      <div className="info-num">
+        <div className="images">
+          <img src={leftImg} />
+          <img src={bottom1} />
+          <img src={bottom2} />
+          <img src={rightImg} />
+          <img src={leftCut} />
+          <img src={bottom1Cut}/>
+          <img src={bottom2Cut}/>
+          <img src={rightCut}/>
+          <img src={nirmaanlogo}/>
+          <img src={logoNir}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeFourthPage;
