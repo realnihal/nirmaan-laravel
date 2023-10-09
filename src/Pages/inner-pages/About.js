66b4e0bd0a38d6c15b6a7e4b28d2c908566dfc8f
shradha biradar <shradhab290@gmail.com>
2023-10-09 17:34:44 +0530

    number in nirman done

--- src/Pages/inner-pages/About.js
+++ src/Pages/inner-pages/About.js
@@ -6,6 +6,8 @@ import NavBar from "./NavBar";
 import Footer from "../Footer";
 import TeamSubCompTitle from "./inner-sub-compoents.js/TeamSubCompTitle";
 import TimeLine from "./inner-sub-compoents.js/TimeLine";
+import nirmanLogo from "../../images/NIRMAAN-LOGO-NEW 1.png";
+import iitmLogo from "../../images/iitmlogo-2.png";
 
 function About() {
   return (
@@ -24,6 +26,39 @@ function About() {
           flag="true"
           subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
         />
+
+        <div className="nirman-numbers">
+          <div className="nir-left">
+            <img src={nirmanLogo} className="a-nir-logo" />
+            <div>
+              <img src={iitmLogo} className="a-iitm-logo" />
+            </div>
+          </div>
+          <div className="numbers-main">
+            <div className="numbers-5">
+              <div className="num-heading">
+                TEAMS TRAINED
+                <div className="num-info trained">141</div>
+              </div>
+              <div className="num-heading">
+                GRADUATED TEAMS
+                <div className="num-info grad">21</div>
+              </div>
+              <div className="num-heading">
+                ACTIVE TEAMS
+                <div className="num-info active">48</div>
+              </div>
+              <div className="num-heading">
+                FUNDS RAISED
+                <div className="num-info funds">60+ Cr</div>
+              </div>
+            </div>
+            <div className="num-heading total-margin">
+              TOTAL VALUATION
+              <div className="num-info total">405+ Cr</div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
