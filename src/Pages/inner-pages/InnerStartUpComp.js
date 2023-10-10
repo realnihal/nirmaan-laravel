43b6468f78ccdb50847850d429277ac074fbe8b9
shradha biradar <shradhab290@gmail.com>
2023-10-11 01:51:42 +0530

    inner-startup second section done

--- src/Pages/inner-pages/InnerStartUpComp.js
+++ src/Pages/inner-pages/InnerStartUpComp.js
@@ -3,6 +3,9 @@ import React from "react";
 import "./innerStartUp.css";
 import linkedIn from "../../images/XMLID_801_ (1).png";
 import img from "../../images/innerStartUpImg.png";
+import mail from "../../images/mail-nir-2.png";
+import TeamSubCompTitle from "./inner-sub-compoents.js/TeamSubCompTitle";
+import MediaSubComp from "./inner-sub-compoents.js/MediaSubComp";
 
 function InnerStartUpComp({
   content,
@@ -11,7 +14,7 @@ function InnerStartUpComp({
   teamMembers,
   subTitle,
   name,
-  inr
+  inr,
 }) {
   return (
     <div className="is-main">
@@ -52,6 +55,32 @@ function InnerStartUpComp({
       </div>
 
       {/* contact us  */}
+      <div className="contact-section">
+        <div className="contact-is">
+          <div className="contact-green"></div>
+          <div className="contact-us-is">CONTACT US</div>
+        </div>
+        <div className="contact-details">
+          <div className="contact-div-1">
+            <div className="mail-img">
+              <img src={mail} />
+            </div>
+            <span className="mail-id">nirmaan@smail.iitm.ac.in</span>
+          </div>
+          <div className="contact-div-2">
+            <a href="https://www.iitm.ac.in/">iitm.ac.in</a>
+          </div>
+          <div className="contact-div-3">
+            <a href="https://www.linkedin.com">
+              <img src={linkedIn} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <TeamSubCompTitle MediaSubtitle="SEE OUR" topic="GALLERY" />
+      <div className="gallery-comp">
+        <MediaSubComp />
+      </div>
     </div>
   );
 }
