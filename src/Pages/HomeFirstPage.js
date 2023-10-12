25b28e348bbaded048ac74ab25352f3c17217fdb
shradha biradar <shradhab290@gmail.com>
2023-10-12 16:19:13 +0530

    teams css changed

--- src/Pages/HomeFirstPage.js
+++ src/Pages/HomeFirstPage.js
@@ -14,36 +14,39 @@ function HomeFirstPage() {
   };
   return (
     <div className="full-page-container">
-      {/* nirman logo */}
       <div className="vertical">WHO WE ARE</div>
-      <div className="nirmaan-logo">
-        <div className="this-is-img">
-          <img src={thisIsImg} />
-        </div>
-        <div className="logo">
-          <img src={nirmaanLogo} />
-        </div>
-        <div className="text subtitle">
-          <div className="sub-heading-first">
-            <div>THIS IS YOUR</div>
-            <div>SUBTITLE</div>
+      {/* nirman logo */}
+      <div className="first-page-div">
+        <div className="nirmaan-logo">
+          <div className="this-is-img">
+            <img src={thisIsImg} />
           </div>
-          <div className="subtitle-content">
-            Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind
-            dedicated pre-incubator on a college campus in the country.
+          <div className="logo">
+            <img src={nirmaanLogo} />
+          </div>
+          <div className="text subtitle">
+            <div className="sub-heading-first">
+              <div>THIS IS YOUR</div>
+              <div>SUBTITLE</div>
+            </div>
+            <div className="subtitle-content">
+              Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind
+              dedicated pre-incubator on a college campus in the country.
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* iit madras logo  */}
-      <div className="iitmlogo">
-        <img src={iitMadrasLogo} />
+        {/* iit madras logo  */}
+        <div className="iitmlogo">
+          <img src={iitMadrasLogo} />
+        </div>
       </div>
       {/* social media  */}
       <div className="social-icons-first">
         <SocialMediaIcons />
       </div>
-      <footer>
+
+      <div className="first-page-footer">
         <div className="vect">
           <img src={vect} />
         </div>
@@ -72,7 +75,7 @@ function HomeFirstPage() {
         <div className="apply" onClick={handleClickApplyTo}>
           <h3>APPLY TO NIRMAAN</h3>
         </div>
-      </footer>
+      </div>
     </div>
   );
 }
--- src/Pages/inner-pages/Teams.js
+++ src/Pages/inner-pages/Teams.js
@@ -12,103 +12,105 @@ function Teams() {
   return (
     <div className="teams-container">
       <NavBar />
-      <img src={teamsVect} className="teams-vect" />
-      <div className="positioning-teams">
-        <div className="main-teams-container">
-          <TeamSubCompTitle
-            subtitle="THIS IS YOUR SUBTITLE"
-            subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
-          />
-        </div>
-        <div className="teams-content margin-1">
-          <TeamsFirstComp
-            name="Name NotName"
-            tagLine="Tagline"
-            className="first-comp-teams-content"
-          />
-          <div className="second-comp-teams-content">
-            <TeamsSecondComp
-              name="Name NotName"
-              designer="Designer"
-              content="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"
+      <div className="teams-wrapper-div">
+        <img src={teamsVect} className="teams-vect" />
+        <div className="positioning-teams">
+          <div className="main-teams-container">
+            <TeamSubCompTitle
+              subtitle="THIS IS YOUR SUBTITLE"
+              subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
             />
           </div>
-          <div className="third-comp-teams-content">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <div className="fourth-comp-teams-content">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          <div className="sixth-comp-teams-content">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-        </div>
-
-        {/* second set ---------------------------- */}
-        <div className="main-teams-container-2">
-          <TeamSubCompTitle
-            subtitle="THIS IS YOUR SUBTITLE"
-            subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
-            flag = "true"
-          />
-        </div>
-
-        <div className="teams-content margin-2">
-          <div className="first-comp-teams-content-2">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <div className="second-comp-teams-content-2">
-            <TeamsSecondComp
+          <div className="teams-content margin-1">
+            <TeamsFirstComp
               name="Name NotName"
-              designer="Designer"
-              content="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"
+              tagLine="Tagline"
+              className="first-comp-teams-content"
+            />
+            <div className="second-comp-teams-content">
+              <TeamsSecondComp
+                name="Name NotName"
+                designer="Designer"
+                content="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"
+              />
+            </div>
+            <div className="third-comp-teams-content">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <div className="fourth-comp-teams-content">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            <div className="sixth-comp-teams-content">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+          </div>
+
+          {/* second set ---------------------------- */}
+          <div className="main-teams-container-2">
+            <TeamSubCompTitle
+              subtitle="THIS IS YOUR SUBTITLE"
+              subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
+              flag="true"
             />
           </div>
-          <div className="third-comp-teams-content-2">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <div className="fourth-comp-teams-content-2">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <div className="fifth-comp-teams-content-2">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <div className="sixth-comp-teams-content-2">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-        </div>
 
-        {/* thirst set-------------------------- */}
+          <div className="teams-content margin-2">
+            <div className="first-comp-teams-content-2">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <div className="second-comp-teams-content-2">
+              <TeamsSecondComp
+                name="Name NotName"
+                designer="Designer"
+                content="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"
+              />
+            </div>
+            <div className="third-comp-teams-content-2">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <div className="fourth-comp-teams-content-2">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <div className="fifth-comp-teams-content-2">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <div className="sixth-comp-teams-content-2">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+          </div>
 
-        <div className="main-teams-container">
-          <TeamSubCompTitle
-            subtitle="THIS IS YOUR SUBTITLE"
-            subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
-          />
-        </div>
-        <div className="teams-content margin-bottom-3">
-          <TeamsFirstComp
-            name="Name NotName"
-            tagLine="Tagline"
-            className="first-comp-teams-content"
-          />
-          <div className="second-comp-teams-content">
-            <TeamsSecondComp
-              name="Name NotName"
-              designer="Designer"
-              content="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"
+          {/* thirst set-------------------------- */}
+
+          <div className="main-teams-container">
+            <TeamSubCompTitle
+              subtitle="THIS IS YOUR SUBTITLE"
+              subContent="Nirmaan, The Pre-incubator, at IIT Madras is the first of its kind dedicated pre-incubator on a college campus in the country."
             />
           </div>
-          <div className="third-comp-teams-content">
+          <div className="teams-content margin-bottom-3">
+            <TeamsFirstComp
+              name="Name NotName"
+              tagLine="Tagline"
+              className="first-comp-teams-content"
+            />
+            <div className="second-comp-teams-content">
+              <TeamsSecondComp
+                name="Name NotName"
+                designer="Designer"
+                content="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"
+              />
+            </div>
+            <div className="third-comp-teams-content">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
+            <div className="fourth-comp-teams-content">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
             <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <div className="fourth-comp-teams-content">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          </div>
-          <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
-          <div className="sixth-comp-teams-content">
-            <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            <div className="sixth-comp-teams-content">
+              <TeamsFirstComp name="Name NotName" tagLine="Tagline" />
+            </div>
           </div>
         </div>
       </div>
