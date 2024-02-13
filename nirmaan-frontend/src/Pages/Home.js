90308e7ae4748a02de515dbcfe6879639840df16
Nihal Puram <puramnihal@gmail.com>
2024-02-13 13:53:27 +0530

    changed some static content

--- nirmaan-frontend/src/Pages/Home.js
+++ nirmaan-frontend/src/Pages/Home.js
@@ -12,25 +12,25 @@ import HomeFourthPage from "./HomeFourthPage";
 import HomeFifthPage from "./HomeFifthPage";
 import HomeSixthPage from "./HomeSixthPage";
 import HomeSeventhPage from "./HomeSeventhPage";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function Home() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 1000);
-  const containerRef = useRef(null);  // ref to the main container
-  const thirdPageRef = useRef(null);  // ref to the third component
-  const sectionsRef = useRef([]);  // ref to array of all sections
+  const containerRef = useRef(null); // ref to the main container
+  const thirdPageRef = useRef(null); // ref to the third component
+  const sectionsRef = useRef([]); // ref to array of all sections
 
   useEffect(() => {
-    const container = containerRef.current;  // current element of main container
-    const sections = sectionsRef.current;  // current array of all sections
-    const thirdPage = thirdPageRef.current;  // current element of the third component
+    const container = containerRef.current; // current element of main container
+    const sections = sectionsRef.current; // current array of all sections
+    const thirdPage = thirdPageRef.current; // current element of the third component
 
     const initializeAnimation = () => {
-      console.log(window.innerHeight)
+      console.log(window.innerHeight);
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: container,
@@ -41,66 +41,104 @@ function Home() {
           scrub: 0.3,
         },
       });
-    
+
       tl.to(container, {
         x: `-${thirdPage.offsetLeft}px`,
         ease: "none",
       })
         .to(thirdPage, {
-          y: () => `-${window.innerHeight + window.innerHeight/2  }px`,
+          y: () =>
+            `-${
+              window.innerHeight
+              // + window.innerHeight/2
+            }px`,
           ease: "power1.inOut",
         })
         .set(thirdPage, { autoAlpha: 0 })
-        .to(sections, {
-          x: () => `-${container.scrollWidth - window.innerWidth - thirdPage.offsetLeft}px`,
-          ease: "none",
-          immediateRender: false,
-        }, "+=0")  // adjust the timing here
+        .to(
+          sections,
+          {
+            x: () =>
+              `-${
+                container.scrollWidth - window.innerWidth - thirdPage.offsetLeft
+              }px`,
+            ease: "none",
+            immediateRender: false,
+          },
+          "+=0"
+        ) // adjust the timing here
         .set(thirdPage, { autoAlpha: 1 }, "<");
     };
 
     // return // for testing ppss
-    if (isMobile) return;  // if mobile device, then return
+    if (isMobile) return; // if mobile device, then return
 
-    window.addEventListener("load", initializeAnimation);  // initialization of animation on page load
+    window.addEventListener("load", initializeAnimation); // initialization of animation on page load
 
     return () => {
-      window.removeEventListener("load", initializeAnimation);  // clearing event worker on unmount of the component
+      window.removeEventListener("load", initializeAnimation); // clearing event worker on unmount of the component
     };
   }, []);
 
-
-
-
   const addRef = (el) => {
-    sectionsRef.current.push(el);  // function for adding ref on each component in the array sectionsRef
+    sectionsRef.current.push(el); // function for adding ref on each component in the array sectionsRef
   };
-
 
   // return <div className="homeFirstPage"><HomeFirstPage /></div>
 
   return isMobile ? (
     <>
-      <div className="homeFirstPage"><HomeFirstPage /></div>
-      <div className="homeSecondPage"><HomeSecondPage /></div>
-      <div className="homeThirdPage"><HomeThirdPage /></div>
-      <div className="homeFourthPage"><HomeFourthPage /></div>
-      <div className="homeFifthPage"><HomeFifthPage /></div>
-      <div className="homeSixthPage"><HomeSixthPage /></div>
-      <div className="homeSeventhPage"><HomeSeventhPage /></div>
+      <div className="homeFirstPage">
+        <HomeFirstPage />
+      </div>
+      <div className="homeSecondPage">
+        <HomeSecondPage />
+      </div>
+      <div className="homeThirdPage">
+        <HomeThirdPage />
+      </div>
+      <div className="homeFourthPage">
+        <HomeFourthPage />
+      </div>
+      <div className="homeFifthPage">
+        <HomeFifthPage />
+      </div>
+      <div className="homeSixthPage">
+        <HomeSixthPage />
+      </div>
+      <div className="homeSeventhPage">
+        <HomeSeventhPage />
+      </div>
     </>
   ) : (
     <div className="container" ref={containerRef}>
-      <div className="panel homeFirstPage" ref={addRef}><HomeFirstPage /></div>
-      <div className="panel homeSecondPage" ref={addRef}><HomeSecondPage /></div>
-      <div className="panel homeThirdPage" ref={thirdPageRef}><HomeThirdPage /></div>
-      <div className="panel homeFourthPage" ref={addRef}><HomeFourthPage /></div>
-      <div className="panel homeFifthPage"
+      <div className="panel homeFirstPage" ref={addRef}>
+        <HomeFirstPage />
+      </div>
+      <div className="panel homeSecondPage" ref={addRef}>
+        <HomeSecondPage />
+      </div>
+      <div className="panel homeThirdPage" ref={thirdPageRef}>
+        <HomeThirdPage />
+      </div>
+      <div className="panel homeFourthPage" ref={addRef}>
+        <HomeFourthPage />
+      </div>
+      <div
+        className="panel homeFifthPage"
         style={{ height: "150vh" }}
-        ref={addRef}><HomeFifthPage /></div>
-      <div className="panel homeSixthPage" ref={addRef}><HomeSixthPage /></div>
-      <div className="panel homeSeventhPage" ref={addRef}><HomeSeventhPage /></div>
-    </div>);
+        ref={addRef}
+      >
+        <HomeFifthPage />
+      </div>
+      <div className="panel homeSixthPage" ref={addRef}>
+        <HomeSixthPage />
+      </div>
+      <div className="panel homeSeventhPage" ref={addRef}>
+        <HomeSeventhPage />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- nirmaan-frontend/src/Pages/sub-components/CarouselSecondPage.js
+++ nirmaan-frontend/src/Pages/sub-components/CarouselSecondPage.js
@@ -1,6 +1,5 @@
-
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import "./carouselSecondPage.css";
 import im1 from "../../images/screen2/Copy of 2F994A92-D1A3-456D-BF5B-08EC031AD064.jpeg";
@@ -13,15 +12,16 @@ import im7 from "../../images/screen2/Copy of Nancy Wang, Scott, Prof. Prabhu.jp
 import im8 from "../../images/screen2/Copy of WhatsApp Image 2023-07-23 at 15.59.25 (1) (1).jpeg";
 import im10 from "../../images/screen2/Copy of WhatsApp Image 2023-07-27 at 18.05.51 (1).jpeg";
 import im11 from "../../images/screen2/Copy of WhatsApp Image 2023-07-30 at 11.37.43 (1).jpeg";
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function CarouselSecondPage() {
-  const [swiperdDirection, setSwiperDirection] = useState(window.innerWidth < 1200 ? 'horizontal' : 'vertical');
+  const [swiperdDirection, setSwiperDirection] = useState(
+    window.innerWidth < 1200 ? "horizontal" : "vertical"
+  );
   const [isVertical, setIsVertical] = useState();
 
   let swiperRef = null;
-  console.log('swiperdDirection', swiperdDirection)
+  console.log("swiperdDirection", swiperdDirection);
 
   const handleSwiper = (swiper) => {
     swiperRef = swiper; // Assign the swiper instance to the swiperRef variable
@@ -38,38 +38,45 @@ function CarouselSecondPage() {
   //   }
   // };
 
-    const setSlidesOffset = (swiper) => {
-      if (swiper) {
-        const isHorizontal = swiper.params.direction === 'horizontal';
+  const setSlidesOffset = (swiper) => {
+    if (swiper) {
+      const isHorizontal = swiper.params.direction === "horizontal";
 
-        const slideDimension = isHorizontal
-          ? swiper.slides[0].offsetWidth // Get the width of the first slide
-          : swiper.slides[0].offsetHeight; // Get the height of the first slide
+      const slideDimension = isHorizontal
+        ? swiper.slides[0].offsetWidth // Get the width of the first slide
+        : swiper.slides[0].offsetHeight; // Get the height of the first slide
 
-        const offset = slideDimension / 5; // Calculate the offset (50% of slide width or height)
-        console.log('isHorizontal', isHorizontal, 'slideDimension', slideDimension, 'offset', offset)
-        if (isHorizontal) {
-          swiper.params.slidesOffsetBefore = -offset; // Set horizontal offset
-          swiper.params.slidesOffsetAfter = offset; // Set horizontal offset after
-        } else {
-          swiper.params.slidesOffsetTop = -offset; // Set vertical offset
-          swiper.params.slidesOffsetBottom = offset; // Set vertical offset bottom
-        }
-
-        swiper.on('slideChange', () => {
-          const lastSlideIndex = swiper.slides.length - 1;
-          const currentSlideIndex = swiper.activeIndex;
-          if (lastSlideIndex - currentSlideIndex <= 3) { // Check if current slide is among the last three
-            swiper.slideTo(0); // Restart Swiper by sliding to the first slide
-          }
-        });
-
-        swiper.update(); // Update Swiper to reflect the changes
+      const offset = slideDimension / 5; // Calculate the offset (50% of slide width or height)
+      console.log(
+        "isHorizontal",
+        isHorizontal,
+        "slideDimension",
+        slideDimension,
+        "offset",
+        offset
+      );
+      if (isHorizontal) {
+        swiper.params.slidesOffsetBefore = -offset; // Set horizontal offset
+        swiper.params.slidesOffsetAfter = offset; // Set horizontal offset after
+      } else {
+        swiper.params.slidesOffsetTop = -offset; // Set vertical offset
+        swiper.params.slidesOffsetBottom = offset; // Set vertical offset bottom
       }
-    };
+
+      swiper.on("slideChange", () => {
+        const lastSlideIndex = swiper.slides.length - 1;
+        const currentSlideIndex = swiper.activeIndex;
+        if (lastSlideIndex - currentSlideIndex <= 4) {
+          // Check if current slide is among the last three
+          swiper.slideTo(0); // Restart Swiper by sliding to the first slide
+        }
+      });
+
+      swiper.update(); // Update Swiper to reflect the changes
+    }
+  };
 
   return (
-
     <Swiper
       spaceBetween={20}
       slidesPerView={2}
@@ -79,16 +86,14 @@ function CarouselSecondPage() {
       }}
       modules={[Autoplay, Pagination, Navigation]}
       navigation={true}
-
       // loop={true}
       // direction={''}
       direction={swiperdDirection}
       // direction={isVertical ? 'vertical' : 'horizontal'}
       onSwiper={handleSwiper}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
       className="vertical-carousel-container"
     >
-
       <SwiperSlide>
         <div className="vertical-carousel__img-box">
           <img src={im1} />
@@ -140,7 +145,6 @@ function CarouselSecondPage() {
         </div>
       </SwiperSlide>
     </Swiper>
-
   );
 }
 
